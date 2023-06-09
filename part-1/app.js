2c01245b6622ef63dc30b9a29d04d90bcbbfe273
shaolun77 <116580308+shaolun77@users.noreply.github.com>
2023-06-08 20:50:37 -0400

    first commit

---
+++ part-1/app.js
@@ -0,0 +1,22 @@
+let favNumber = 3;
+let baseURL = "http://numbersapi.com";
+
+// 1.
+$.getJSON(`${baseURL}/${favNumber}?json`).then(data => {
+  console.log(data);
+});
+
+// 2.
+let favNumbers = [7, 8, 23];
+$.getJSON(`${baseURL}/${favNumbers}?json`).then(data => {
+  console.log(data);
+});
+
+// 3.
+Promise.all(
+  Array.from({ length: 4 }, () => {
+    return $.getJSON(`${baseURL}/${favNumber}?json`);
+  })
+).then(facts => {
+  facts.forEach(data => $("body").append(`<p>${data.text}</p>`));
+});
